c7275ee1b0653e5edce3baeaf53ad4694c834ab0
A Machalica <a.machalica@selleo.com>
2017-08-25 23:50:18 +0200

    Implements function for getting random emoji and uses twemoji package

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import twemoji from 'twemoji'
+
+import { Emojis } from './assets/emojis.js';
 import './App.css';
 
 class App extends Component {
+  state = {
+    index: 0,
+  };
+
+  getRandomEmoji = () => {
+    this.setState({
+      index: Math.floor(Math.random() * 200)
+    });
+  };
+
+  displayEmoji() {
+    const selectedEmoji = Emojis[this.state.index];
+    const twemojiMarkup = {
+      __html: twemoji.parse(selectedEmoji.emoji)
+    };
+
+    return (
+      <div className="test">
+        <div>emoji: {selectedEmoji.emoji}</div>
+        <div>codepoint: {selectedEmoji.codepoint}</div>
+        <div>parsed codepoint: {String.fromCodePoint(selectedEmoji.codepoint)}</div>
+        <div>twemoji: <div dangerouslySetInnerHTML={twemojiMarkup}/></div>
+        <div>index: {this.state.index}</div>
+      </div>
+    )
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Welcome to Emoji Routlette</h2>
+        <button onClick={this.getRandomEmoji}>Get new emoji</button>
+        {
+          (this.state.index < Emojis.length)
+            ? this.displayEmoji()
+            : <div>No emoji for index: {this.state.index}</div>
+        }
       </div>
     );
   }
