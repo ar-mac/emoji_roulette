693cced70bab43e84fc8521a2e1309590bb053f7
A Machalica <a.machalica@selleo.com>
2017-09-12 21:32:42 +0200

    pd_implementation

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import DisplayEmoji from './assets/DisplayEmoji';
 import { Emojis } from './assets/emojis.js';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   state = {
     index: 0,
   };
 
   getRandomEmoji = () => {
     this.setState({
-      index: Math.floor(Math.random() * 200)
+      index: Math.floor(Math.random() * 80)
+    });
+  };
+
+  resetIndex = () => {
+    this.setState({
+      index: 0
     });
   };
 
@@ -21,12 +27,10 @@ class App extends Component {
         <h2>Welcome to Emoji Routlette</h2>
         <button onClick={this.getRandomEmoji}>Get new emoji</button>
         {(this.state.index < Emojis.length)
-          ? <DisplayEmoji index={this.state.index} />
+          ? <DisplayEmoji index={this.state.index} resetFn={this.resetIndex} key={this.state.index}   />
           : <div>No emoji for index: {this.state.index}</div>}
 
       </div>
     );
   }
 }
-
-export default App;
--- src/assets/DisplayEmoji.js
+++ src/assets/DisplayEmoji.js
@@ -4,21 +4,39 @@ import PropTypes from 'proptypes';
 
 import { Emojis } from './emojis.js';
 
-const DisplayEmoji = ({ index }) => {
-  const selectedEmoji = Emojis[index];
-  const twemojiMarkup = {
-    __html: twemoji.parse(selectedEmoji.emoji)
+class DisplayEmoji extends React.Component {
+  state = {
+    secondsPassed: 0,
   };
+  componentDidMount = () => {
+   this.intervalId =  setInterval(() => this.setState({secondsPassed: this.state.secondsPassed+1}), 1000)
+  }
+  componentWillUnmount = () => {
+    clearInterval(this.intervalId)
+  }
+  render (){
+    const { index, resetFn } = this.props;
 
-  return (
-    <div className="test">
-      <div>emoji: {selectedEmoji.emoji}</div>
-      <div>codepoint: {selectedEmoji.codepoint}</div>
-      <div>parsed codepoint: {String.fromCodePoint(selectedEmoji.codepoint)}</div>
-      <div>twemoji: <div dangerouslySetInnerHTML={twemojiMarkup}/></div>
-      <div>index: {this.state.index}</div>
-    </div>
-  )
+    const selectedEmoji = Emojis[index];
+    const twemojiMarkup = {
+      __html: twemoji.parse(selectedEmoji.emoji)
+    };
+
+    return (
+      <div className="test">
+        <div>emoji: {selectedEmoji.emoji}</div>
+        <div>codepoint: {selectedEmoji.codepoint}</div>
+        <div>parsed codepoint: {String.fromCodePoint(selectedEmoji.codepoint)}</div>
+        <div>Seconds Passed: {this.state.secondsPassed}</div>
+        <div>twemoji: <div dangerouslySetInnerHTML={twemojiMarkup}/></div>
+        <div>index: {index}</div>
+        <button onClick={resetFn}>Reset index</button>
+
+      </div>
+
+    )
+  }
+
 };
 
 DisplayEmoji.propTypes = {
