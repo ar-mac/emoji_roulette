8f5b423449aff69a9337560f48226e945e1b4f1d
A Machalica <a.machalica@selleo.com>
2017-12-14 17:03:51 +0100

    Specs for rendering form markup

--- src/components/RegisterForm/__test__/RegisterForm.test.js
+++ src/components/RegisterForm/__test__/RegisterForm.test.js
@@ -12,4 +12,35 @@ describe('RegisterForm', () => {
   test('renders with default props', () => {
     expect(setup().wrapper).toMatchSnapshot();
   });
+
+  test('renders whole state correctly', () => {
+    const { wrapper } = setup();
+    const state = {
+      data: {
+        age: '45',
+        username: 'user1',
+        email: 'some failing',
+        addresses: [
+          { city: 'city1', zipCode: '01-001' },
+          { city: 'city2', zipCode: '07-007' },
+        ],
+        errors: {
+          email: 'is not correct',
+        },
+        isSending: false,
+      }
+    };
+  });
+
+  test('renders multiple addresses', () => {
+    const { wrapper } = setup();
+    const addresses = [
+      { city: 'city1', zipCode: '01-001' },
+      { city: 'city2', zipCode: '07-007' },
+    ];
+    const initialState = wrapper.state();
+    initialState.data.addresses = addresses;
+    wrapper.setState(initialState);
+    expect(wrapper.instance()).toMatchSnapshot();
+  });
 });
