67f05c2a8c69b1fa293de39491f21fe2ffa310e0
A Machalica <a.machalica@selleo.com>
2018-03-14 23:28:41 +0100

    Add boilerplate for draws resource

---
+++ src/store/draws/reducer.js
@@ -0,0 +1,14 @@
+import * as types from './types';
+
+const initialState = {
+};
+
+function drawReducer(state = initialState, action) {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
+
+export default drawReducer;
