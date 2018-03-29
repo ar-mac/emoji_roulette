8c3d9e3c3a12fcd9d9024f8f476618db5005da8a
A Machalica <a.machalica@selleo.com>
2018-03-29 16:51:54 +0200

    New way of implementing reducer

--- src/store/draws/reducer.js
+++ src/store/draws/reducer.js
@@ -1,3 +1,4 @@
+import { cloneDeep } from 'lodash';
 import * as types from './types';
 
 const initialState = {
@@ -9,15 +10,21 @@ const initialState = {
 function drawReducer(state = initialState, action) {
   switch (action.type) {
     case types.SET_NEW: {
-      return {
-        ...state,
-        byId: {
-          ...state.byId,
-          [action.payload.draw.id]: action.payload.draw,
-        },
-        selectedDrawId: action.payload.draw.id,
-        previousDrawIds: [action.payload.draw.id, ...state.previousDrawIds].slice(0, 9)
-      };
+      const newState = cloneDeep(state);
+      newState.byId[action.payload.draw.id] = action.payload.draw;
+      newState.selectedDrawId = action.payload.draw.id;
+      newState.previousDrawIds = [action.payload.draw.id, ...state.previousDrawIds].slice(0, 9)
+      return newState;
+
+      // return {
+      //   ...state,
+      //   byId: {
+      //     ...state.byId,
+      //     [action.payload.draw.id]: action.payload.draw,
+      //   },
+      //   selectedDrawId: action.payload.draw.id,
+      //   previousDrawIds: [action.payload.draw.id, ...state.previousDrawIds].slice(0, 9)
+      // };
     }
     default: {
       return state;
