95e973bb5e94e7e0c6699a9b16f8dce4712d61bf
A Machalica <a.machalica@selleo.com>
2018-05-24 08:23:24 +0200

    Setup action creators for setupDraws functionality

--- src/store/jokes/actionCreators.js
+++ src/store/jokes/actionCreators.js
@@ -1,2 +1,7 @@
 export const getRandomJoke = () =>
   fetch('https://api.chucknorris.io/jokes/random').then((response) => response.json());
+
+export const getJoke = () => {
+//  implement action creator for fetching specific joke by ID
+//  it's needed for `setupDraws` action creator
+};
